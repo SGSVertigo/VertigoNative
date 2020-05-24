c08b221116fd30bd49cee4e4cfd077b1bc33adb0
Ben Oxley <ben.oxley@gmail.com>
2020-05-24 18:49:13 +0100

    Added material design library and working bluetooth connection

--- App.tsx
+++ App.tsx
@@ -26,6 +26,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import BtControls from './BtControls';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 
 
@@ -34,66 +35,24 @@ declare const global: {HermesInternal: null | {}};
 
 const App = () => {
   return (
+    <PaperProvider>
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
+          contentInsetAdjustmentBehavior="automatic">
+         
+          <View>
+            <View>
               <BtControls></BtControls>
             </View>
           </View>
         </ScrollView>
       </SafeAreaView>
     </>
+    </PaperProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
--- BtControls.tsx
+++ BtControls.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, FlatList, TouchableHighlight } from 'react-native';
 import { BleManager, Device } from 'react-native-ble-plx';
+import { List, Appbar } from 'react-native-paper';
+import App from 'App';
 
 type BluetoothProps = {
 
@@ -8,13 +10,32 @@ type BluetoothProps = {
 
 const vertigoDataServiceId: string = "d7a7fc0a-b32e-4bda-933f-49cbd9cfe2dc";
 const vertigoHardwareServiceId: string = "0000180a-0000-1000-8000-00805f9b34fb";
+//Serial Number
+const serialNumberCharateristicID: string = '00002a25-0000-1000-8000-00805f9b34fb';
+//Firmware version 
+const firwareRevisionCharateristicID: string = '00002a26-0000-1000-8000-00805f9b34fb';
+// Altitude & Heading Reference System (“AHRS”)
+const imuQuaternionCharacteristicID: string = '45ae0807-2233-4026-b264-045a933fa973';
+// Magnetometer/Accelerometer/Rate Gyro (“MARG”)
+const magnetometerCharacteristicID: string = '45ae0807-2233-4026-b264-045a933fa974';
+// Atmospheric / Ambient sensors (“ATMO”)
+const atmosphericCharacteristicID: string = '45ae0807-2233-4026-b264-045a933fa975';
+// Global Navigation Satellite System (“GNSS”)
+const gpsCharacteristicID: string = 'f078622c-f2ee-4adb-896f-cef6645e1521';
+// STATUS (“STAT”)
+const statusCharacteristicID: string = 'c70617b6-993d-481f-b02a-7fcfbb3d2133';
+// CONTROL (“CTRL”)
+const controlCharacteristicID: string = 'c771b990-055f-11e9-8eb2-f2801f1b9fd1';
 
 export default class BtControls extends Component<BluetoothProps> {
 
     manager: BleManager;
     devices: Device[] = [];
+    selectedDevice: Device | null = null;
     state = {
+        selectedDevice: this.selectedDevice,
         scanning: false,
+        logging: false,
         bluetoothReady: false,
         manager: null,
         devices: this.devices
@@ -71,17 +92,12 @@ export default class BtControls extends Component<BluetoothProps> {
         });
     }
 
-    connect(device: Device) {
-        device.connect()
-            .then((device) => {
-                return device.discoverAllServicesAndCharacteristics()
-            })
-            .then((device) => {
-                // Do work on device with services and characteristics
-            })
-            .catch((error) => {
-                // Handle errors
-            });
+    selectDevice(device: Device) {
+        this.manager.stopDeviceScan();
+        this.setState({
+            scanning: false,
+            selectedDevice: device
+        });
     }
 
     private getLoggingStatus(): string {
@@ -90,23 +106,49 @@ export default class BtControls extends Component<BluetoothProps> {
         return "Scan";
     }
 
+    private getScanScreen() {
+        return (<View>
+            <Button title={this.getLoggingStatus()} disabled={!this.state.bluetoothReady} onPress={this.toggleLogging.bind(this)}></Button>
+            <FlatList
+                data={this.state.devices}
+                renderItem={({ item }) => <TouchableHighlight onPress={(e) => this.selectDevice(item)} key={item.id}>
+                    <List.Item
+                        title={item.name ? item.name : "Unknown Device"}
+                        description={item.id}
+                        left={props => <List.Icon {...props} icon="bluetooth" />}
+                    />
+
+                </TouchableHighlight>
+                }
+            />
+        </View>);
+    }
+
+    private getDevice() {
+        return (<View>
+            <Appbar.Header>
+                <Appbar.BackAction onPress={() => this.back()} />
+                <Appbar.Content title={this.state.selectedDevice?.name} subtitle={this.state.selectedDevice?.id} />
+            </Appbar.Header>
+            <Button title="Connect" onPress={this.connect.bind(this)}></Button>
+            <Button title="Start Logging" onPress={() => this.startLogging()}></Button>
+            <Button title="Stop Logging" onPress={() => this.stopLogging()}></Button>
+            <Button title="Calibrate" onPress={() => this.stopLogging()}></Button>
+        </View>);
+    }
+
+    back() {
+        this.setState({
+            selectedDevice: null
+        });
+    }
+
     render() {
         return (
-            <View>
-                <Button title={this.getLoggingStatus()} disabled={!this.state.bluetoothReady} onPress={this.toggleLogging.bind(this)}></Button>
-                <FlatList
-                    data={this.state.devices}
-                    renderItem={({ item }) => <TouchableHighlight onPress={(e) => this.connect(item)} key={item.id}>
-                        <View>
-                            <View>
-                                <Text >{item.name}</Text>
-                                <Text >{item.id}</Text>
-                            </View>
-                        </View>
 
-                    </TouchableHighlight>
-                    }
-                />
+            <View>
+
+                {this.state.selectedDevice ? this.getDevice() : this.getScanScreen()}
             </View>
 
         );
@@ -114,6 +156,56 @@ export default class BtControls extends Component<BluetoothProps> {
 
     componentWillUnmount() {
         this.manager.destroy();
+    }
+
+    public connect() {
+        if (this.state.selectedDevice){
+            this.manager.destroy();
+            this.manager = new BleManager();
+            console.debug("Connecting");
+            this.selectedDevice?.connect()
+                .then((device) => {
+                    console.debug("Connected");
+                    return device.discoverAllServicesAndCharacteristics()
+                })
+                .then((device) => {
+                // Do work on device with services and characteristics
+                })
+                .catch((error) => {
+                    console.debug("Error");
+                    console.error(error.message)
+                });
+        }
+        
+    }
+
+    public startLogging() {
+        this.setState({
+            logging: true
+        });
+        if (this.state.selectedDevice?.isConnected()) {
+            console.debug("Is connected Connected");
+            //this.manager.writeCharacteristicWithoutResponseForDevice(this.state.selectedDevice.id,vertigoDataServiceId,controlCharacteristicID,"AQ==").catch(error=>console.error(error));
+            this.state.selectedDevice?.writeCharacteristicWithoutResponseForService(vertigoDataServiceId,controlCharacteristicID,"AQ==").catch(error=>console.error(error));
+        } else {
+            console.error("Not connected");
+        }
+    }
+
+
+    public stopLogging() {
+        this.setState({
+            logging: false
+        });
+        if (this.state.selectedDevice?.isConnected()) {
+            this.state.selectedDevice?.writeCharacteristicWithoutResponseForService(vertigoDataServiceId,controlCharacteristicID,"Ag==").catch(error=>console.error(error));
+        }
+    }
+
+    public calibrate() {
+        if (this.state.selectedDevice?.isConnected()) {
+            this.state.selectedDevice?.writeCharacteristicWithoutResponseForService(vertigoDataServiceId,controlCharacteristicID,"CA==").catch(error=>console.error(error));
+        }
     }
 
 
